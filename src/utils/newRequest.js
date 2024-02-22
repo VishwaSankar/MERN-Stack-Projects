2ab25327afd28692df14da42f778917ab8514dd0
VishwaSankar <136370992+VishwaSankar@users.noreply.github.com>
2024-02-22 15:09:27 +0530

    Update newRequest.js

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-    baseURL:"https://backends-k9wj.onrender.com", withCredentials:true, credentials: 'include'
+    baseURL:"https://backends-k9wj.onrender.com",  credentials: 'include'
 })
 
 export default newRequest
